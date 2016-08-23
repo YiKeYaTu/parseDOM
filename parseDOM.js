2f80cdb87e33ade498dc845fae14ac2d84edbaec
YiKeYaTu <liangchen005005@163.com>
2016-08-23 14:35:20 +0800

    将DOM树解析成对象节点

--- parseDOM.js
+++ parseDOM.js
@@ -6,90 +6,105 @@ let html = `
         <title></title>
     </head>
     <body>2121
-        <div id="caonima">
+        <div id="caonima" hh="sa">
+            <!-- saf -->
             <p class="ca" data="<div id=''></div>"><span></span></p>
         </div>
     </body>
     </html>
 `;
 
-function parseDOM (html) {
-    this.html = html;
-    this.attr = {};
-    this.note = {};
+class DOM {
 
-    this.nodeTree = [];
+    constructor (domTree) {
+        super();
+        this.domTree = domTree;
+    }
 
-    this._keepAttr();
-    this._matchTag();
-} 
+    _walk (fn/*, startDOM*/) {
 
-parseDOM.prototype._matchTag = function () {
-    const TAG_EXP = /<\s*[\/a-zA-Z]+(0\.\d+)?\s*>/g,
-          TAG_NAME_EXP = /[A-Za-z]+/,
-          RAND_STR_EXP = /0\.\d+/,
-          END_TAG_EXP = /<\s*\/[a-zA-Z]+\s*>/;
-
-    let treeArray = [];
-    let n;
-
-    this.html = this.html.replace(TAG_EXP, (str) => {
-        let strArray = str.match(RAND_STR_EXP);
-        let tagName = str.match(TAG_NAME_EXP)[0],
-            attr = this.attr[strArray && strArray[0]];
-
-        if (str.match(END_TAG_EXP)) {
-            treeArray.pop();
-            n = treeArray[treeArray.length - 1];
-            return str;
+        if (arguments[1]) {
+            
         }
 
-        let node = {
-            tagName: tagName,
-            tagHash: this.roundStr(),
-            attr: attr,
-            children: [],
-        };
-        if (!n) {
-            n = node;
-            this.nodeTree.push(n);
-        } else {
-            n.children.push(node);
+    }
+
+    getElementById (id) {
+
+    }
+
+}
+
+class parseDOM {
+
+    constructor (htmlString) {
+        super();
+        this.htmlString = htmlString;
+        this.DOM = this._match();
+        return new DOM(this.DOM);
+    }
+
+    _match () {
+        const TAG_EXP = /<(?:[a-zA-Z]+?)\s*(?:.+?\s*=\s*(?:(?:'.+?')|(?:".+?"))\s*)*>|<\/[a-zA-Z]+\s*>/g,
+              NODENAME_EXP = /<([a-zA-Z]+)\s*/,
+              ATTR_EXP = /([^<>\s\n]+)\s*=\s*((?:'.+?')|(?:".+?"))/g,
+              END_EXP = /^<\/[a-zA-Z]+\s*>/;
+
+        let tagArr = [];
+
+        let nodeName, attr, nodeElement;
+
+        let domTree = [], n;
+
+        this.htmlString.replace(TAG_EXP, (item, index) => {
+            if (item.match(END_EXP)) {
+                nodeElement = tagArr.pop();
+                if (nodeElement) nodeElement.innerHTML = this.htmlString.slice(nodeElement.index, index);
+                n = tagArr[tagArr.length - 1];
+                return item;
+            }
+
+            nodeName = item.match(NODENAME_EXP)[1];
+            attr = item.match(ATTR_EXP);
+
+            attr && (attr = attr.map((item) => {
+                return this._parseAttr(item);
+            }));
+
+            nodeElement = {
+                nodeName,
+                attr,
+                index: index + item.length,
+                children: []
+            };
+
+
+            tagArr.push(nodeElement);
+
+            if (!n) {
+                domTree.push(nodeElement);
+            } else {
+                n.children.push(nodeElement);
+            }
+            n = nodeElement;
+
+            return item;
+        });
+
+        return domTree;
+    }
+
+    _parseAttr (attrString) {
+        const VAL_KEY = /(.+?)=((?:'.+')|(?:".+"))/;
+        let match = attrString.match(VAL_KEY);
+        return {
+            key: match[1],
+            value: match[2],
         }
-        treeArray.push(node);
-        n = treeArray[treeArray.length - 1];
-        return str.replace(RAND_STR_EXP, (str) => {
-            return this.attr[str];
-        });
-    })
-    console.log(this.html);
+    }
 
 }
 
-parseDOM.prototype._keepAttr = function () {
-    const TAG_EXP = /<.+?\s+([^\s=]+\s*=\s*(('.+?')|(".+?"))\s*)+?>/g,
-          ATTR_EXP = /\s+([^\s=]+\s*=\s*(('.+?')|(".+?"))\s*)+/g;
 
-    let randomStr;
-
-    this.html = this.html.replace(TAG_EXP, (str) => {
-        return str.replace(ATTR_EXP, (match) => {
-            randomStr = this.roundStr();
-            this.attr[randomStr] = match;
-            return randomStr;
-        });
-    });
-    // console.log(this.attr);
-    // console.log(this.html);
-}
-
-parseDOM.prototype._keepNote = function () {
-
-}
-
-parseDOM.prototype.roundStr = function () {
-    let str = Math.random();
-    return (this.attr[str] || this.html.indexOf(str) > -1) ? this.roundStr() : str;
-}
 
 let tree = new parseDOM(html);
